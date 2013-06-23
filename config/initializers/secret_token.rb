03d72a9748055f32bd1f90b27e772bcd77664c6f
Jonathan Choo <jonathanchoo@live.co.uk>
2013-06-23 17:42:25 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeethungApp::Application.config.secret_token = '7f0993402b3c4b105622b32a5e9aceae484bd42483c383141584aeb5cf377cde1f382a06974cb5c203afc27c239e38be286f561aa9d621222f34648f6ae15a2b'
